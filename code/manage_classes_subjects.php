fb8f7571ea9123725df6d8234eaf8cb9e40f1405
Defendicon786 <Hassan.tariq771@gmail.com>
2025-06-10 10:59:56 +0500

    Add dark mode toggle to navbars

--- code/manage_classes_subjects.php
+++ code/manage_classes_subjects.php
@@ -752,6 +752,11 @@ $stmt->close();
                             <i class="material-icons">power_settings_new</i> Log Out
                         </a>
                     </li>
+          <li class="nav-item d-flex align-items-center">
+            <div class="form-check form-switch mb-0">
+              <input class="form-check-input" type="checkbox" id="darkModeToggle">
+            </div>
+          </li>
                 </ul>
             </div>
         </div>
--- code/manage_notifications.php
+++ code/manage_notifications.php
@@ -228,6 +228,11 @@
               <i class="material-icons">power_settings_new</i> Log Out
             </a>
           </li>
+          <li class="nav-item d-flex align-items-center">
+            <div class="form-check form-switch mb-0">
+              <input class="form-check-input" type="checkbox" id="darkModeToggle">
+            </div>
+          </li>
         </ul>
       </div>
     </div>
