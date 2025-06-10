92854f2e81b95ab8da1a1200c7dec8318740aeb7
Defendicon786 <Hassan.tariq771@gmail.com>
2025-06-10 10:49:59 +0500

    Fix dark mode toggle placement

--- code/assets/js/dark-mode.js
+++ code/assets/js/dark-mode.js
@@ -6,14 +6,25 @@ function initDarkMode(){
     link.id='dark-mode-style';
     document.head.appendChild(link);
   }
-  const nav=document.querySelector('.navbar .navbar-nav');
-  if(nav && !document.getElementById('darkModeToggle')){
-    const li=document.createElement('li');
-    li.className='nav-item d-flex align-items-center';
-    li.innerHTML='<div class="form-check form-switch mb-0">'+
-                 '<input class="form-check-input" type="checkbox" id="darkModeToggle">'+
-                 '</div>';
-    nav.appendChild(li);
+  const navList=document.querySelector('.navbar .navbar-nav');
+  const navContainer=document.querySelector('.navbar .container');
+  if(!document.getElementById('darkModeToggle')){
+    if(navList){
+      const li=document.createElement('li');
+      li.className='nav-item d-flex align-items-center';
+      li.innerHTML='<div class="form-check form-switch mb-0">'+
+                   '<input class="form-check-input" type="checkbox" id="darkModeToggle">'+
+                   '</div>';
+      navList.appendChild(li);
+    } else if(navContainer){
+      const div=document.createElement('div');
+      div.className='d-flex align-items-center ml-auto';
+      div.style.marginLeft='auto';
+      div.innerHTML='<div class="form-check form-switch mb-0">'+
+                    '<input class="form-check-input" type="checkbox" id="darkModeToggle">'+
+                    '</div>';
+      navContainer.appendChild(div);
+    }
   }
   const toggle=document.getElementById('darkModeToggle');
   if(!toggle) return;
