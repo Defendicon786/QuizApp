d928d459cc18cc171470756085bb4e2aed808309
Defendicon786 <Hassan.tariq771@gmail.com>
2025-09-02 22:44:27 +0500

    Include Material Dashboard JS on results page

--- code/my_results.php
+++ code/my_results.php
@@ -168,6 +168,8 @@ $results = $stmt->get_result();
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="./assets/js/dark-mode.js"></script>
+    <!-- Material Dashboard JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@material/material-dashboard@1.0.0/dist/js/material-dashboard.min.js"></script>
+    <script src="./assets/js/dark-mode.js"></script>
 </body>
-</html> 
+</html>
