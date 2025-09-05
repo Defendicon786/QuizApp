433d9a0f3a302d952deab6fd9d05ece03e60849c
Defendicon786 <Hassan.tariq771@gmail.com>
2025-09-05 20:24:37 +0500

    Relocate logo upload above Add User button

--- code/paper_manage.php
+++ code/paper_manage.php
@@ -149,7 +149,9 @@ $conn->close();
                         <div class="form-group">
                           <label class="bmd-label-floating">Freeze Date</label>
                           <input type="date" name="expires_on" class="form-control">
-                          <input type="file" name="logo" id="logoInput" class="form-control" accept="image/*" style="margin-top:10px;">
+                        </div>
+                        <div class="form-group">
+                          <input type="file" name="logo" id="logoInput" class="form-control" accept="image/*">
                           <img id="logoPreview" alt="Logo Preview" style="max-height:100px; display:none; margin-top:10px;" />
                         </div>
                         <button type="submit" class="btn btn-primary pull-right">Add User</button>
